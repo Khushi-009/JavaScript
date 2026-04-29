84a60b54478d2a6e5e52ebd2e1addb0972e5f80f
Khushi Arya <arya2210118@akgec.ac.in>
2026-04-29 11:52:29 +0000

    dataTypes committed

---
+++ 01_bascis/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs not browser
+
+console.log(3+3)  // code readiability is must 
+
+console.log("khushi")
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  --> type of null is object and undefined is undefined
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof null)  // object
+console.log(typeof undefined)  // undefined
