b2d93d3fa5cd6d5ffc5a02e0b92bdf699b640c16
Khushi Arya <arya2210118@akgec.ac.in>
2026-04-29 18:27:31 +0000

    Conversion operation done

--- 01_bascis/03_conversionOperations.js
+++ 01_bascis/03_conversionOperations.js
@@ -1,7 +1,33 @@
-let score = "33"  // if the number is string
+// let score = "33abc"  // if the number is string
+let score = null
 
 console.log(typeof score)
 console.log(typeof(score))
 
 let valueInNumber = Number(score)  // type conversion
-console.log(typeof valueInNumber);
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1; false=0
+
+let aBooleanNumber = "khushi"
+
+let convertedBoolean = Boolean(aBooleanNumber)   // conversion of boolean
+
+console.log(convertedBoolean)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let someString = String(someNumber)
+
+console.log(someString)
+console.log(typeof someString);
+
