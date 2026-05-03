15aead8d8907213bfd6571995d1f11c9a9aace93
Khushi Arya <arya2210118@akgec.ac.in>
2026-05-03 11:04:44 +0000

    strings done

---
+++ 01_bascis/05_strings.js
@@ -0,0 +1,28 @@
+const name = "khushi"
+const repoCount = 15
+
+// console.log(name + repoCount + " value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);  // string interpletaion
+
+const gameName = new String('khushi')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// console.log(gameName.substring(0,4))
+// console.log(gameName.slice(-6,4));
+
+
+const newString = "     khushi    "
+
+console.log(newString);
+console.log(newString.trim());
+
+//  replace , include, split-> to convert into array 
